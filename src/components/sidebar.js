afab8c394ad7afd4eb0e64acf87d0d42e277fe16
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2024-02-28 22:38:30 -0500

    sidebar container

---
+++ src/components/sidebar.js
@@ -0,0 +1,10 @@
+import { Box } from '@mui/material';
+import Tags from './tags';
+
+export default function Sidebar() {
+  return (
+    <Box component='aside'>
+      <Tags />
+    </Box>
+  );
+}
