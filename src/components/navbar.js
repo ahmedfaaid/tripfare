0c221c54bccfabaea71eae5d9c177e71484b76bd
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2024-02-28 19:37:50 -0500

    navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,23 +1,72 @@
 'use client';
-import AdbIcon from '@mui/icons-material/Adb';
+import BorderColorIcon from '@mui/icons-material/BorderColor';
+import HouseIcon from '@mui/icons-material/House';
 import MenuIcon from '@mui/icons-material/Menu';
+import PersonIcon from '@mui/icons-material/Person';
+import SearchIcon from '@mui/icons-material/Search';
 import {
   AppBar,
   Box,
+  Button,
   Container,
   IconButton,
   Toolbar,
-  Typography
+  Typography,
+  alpha,
+  styled
 } from '@mui/material';
 import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
+import InputBase from '@mui/material/InputBase';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import Tooltip from '@mui/material/Tooltip';
+import { grey } from '@mui/material/colors';
 import { useState } from 'react';
 
 const pages = ['Products', 'Pricing', 'Blog'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: 20,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25)
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: '100%',
+  display: 'none',
+  [theme.breakpoints.up('md')]: {
+    marginLeft: theme.spacing(3),
+    width: 'auto',
+    display: 'block'
+  }
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 1),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center'
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: grey[50],
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch'
+    }
+  }
+}));
 
 export default function Navbar() {
   const [anchorElNav, setAnchorElNav] = useState(null);
@@ -39,99 +88,109 @@ export default function Navbar() {
   };
 
   return (
-    <AppBar position='static'>
-      <Container maxWidth='xl'>
+    <AppBar position='static' color='tripfare'>
+      <Container maxWidth='lg'>
         <Toolbar disableGutters>
-          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          <Button
+            href='/'
+            sx={{
+              display: { xs: 'none', md: 'flex' },
+              mr: 3,
+              color: grey[50]
+            }}
+          >
+            <HouseIcon />
+          </Button>
           <Typography
-            variant='h6'
+            variant='h5'
             noWrap
             component='a'
-            href='#app-bar-with-responsive-menu'
+            href='/'
             sx={{
               mr: 2,
               display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
               fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
+              color: grey[50],
               textDecoration: 'none'
             }}
           >
-            LOGO
+            TripFare
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
             <IconButton
-              size='large'
               aria-label='account of current user'
               aria-controls='menu-appbar'
               aria-haspopup='true'
               onClick={handleOpenNavMenu}
               color='inherit'
             >
-              <MenuIcon />
+              <MenuIcon sx={{ color: grey[50] }} />
             </IconButton>
-            <Menu
-              id='menu-appbar'
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left'
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left'
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' }
-              }}
-            >
-              {pages.map(page => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign='center'>{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
+            <Search>
+              <SearchIconWrapper>
+                <SearchIcon />
+              </SearchIconWrapper>
+              <StyledInputBase
+                placeholder='Search…'
+                inputProps={{ 'aria-label': 'search' }}
+              />
+            </Search>
           </Box>
-          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
           <Typography
             variant='h5'
             noWrap
             component='a'
-            href='#app-bar-with-responsive-menu'
+            href='/'
             sx={{
               mr: 2,
               display: { xs: 'flex', md: 'none' },
               flexGrow: 1,
-              fontFamily: 'monospace',
               fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none'
+              color: grey[50],
+              textDecoration: 'none',
+              textAlign: 'center'
             }}
           >
-            LOGO
+            TripFare
           </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {pages.map(page => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}
-              >
-                {page}
-              </Button>
-            ))}
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: { xs: 'none', md: 'flex' },
+              justifyContent: 'end',
+              alignItems: 'center'
+            }}
+          >
+            <Search>
+              <SearchIconWrapper>
+                <SearchIcon sx={{ color: grey[50] }} />
+              </SearchIconWrapper>
+              <StyledInputBase
+                placeholder='Search…'
+                inputProps={{ 'aria-label': 'search' }}
+              />
+            </Search>
+            <BorderColorIcon
+              fontSize='medium'
+              sx={{
+                display: { xs: 'none', md: 'flex' },
+                mx: 3,
+                color: grey[50]
+              }}
+            />
           </Box>
 
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title='Open settings'>
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt='Remy Sharp' src='/static/images/avatar/2.jpg' />
+              <IconButton
+                onClick={handleOpenUserMenu}
+                aria-controls='menu-appbar'
+                aria-haspopup='true'
+              >
+                <Avatar sx={{ bgcolor: grey[50], width: 24, height: 24 }}>
+                  <PersonIcon fontSize='small' color='tripfare' />
+                </Avatar>
               </IconButton>
             </Tooltip>
             <Menu
