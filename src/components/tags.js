9e644391ca3eb00a88d5c98c58706a3b1a747155
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2024-02-28 22:38:38 -0500

    tags component

---
+++ src/components/tags.js
@@ -0,0 +1,27 @@
+import { Box, Link, List, ListItem } from '@mui/material';
+
+const tags = [
+  'Popular Posts',
+  'Trending Posts',
+  'Solo Travel',
+  'Couple Travel',
+  'Group Travel',
+  'Popular Destinations',
+  'Trending Destinations'
+];
+
+export default function Tags() {
+  return (
+    <Box sx={{ boxShadow: 1, borderRadius: 2 }}>
+      <List>
+        {tags.map(tag => (
+          <ListItem key={tag}>
+            <Link href='#' underline='hover' color='soot'>
+              {tag}
+            </Link>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+}
