d00ed153269b615d2f569dce7bc244bcf7c52ab7
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2024-02-29 10:56:39 -0500

    add followers box

---
+++ src/components/followers.js
@@ -0,0 +1,51 @@
+import {
+  Avatar,
+  Box,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemButton,
+  ListItemText
+} from '@mui/material';
+
+const followers = [
+  {
+    id: 1,
+    username: 'User_3240',
+    image: '/images/pexels-dziana-hasanbekava-7275385.jpg'
+  },
+  {
+    id: 2,
+    username: 'User_3498',
+    image: '/images/pexels-italo-melo-2379004.jpg'
+  },
+  {
+    id: 3,
+    username: 'Coco Chanel',
+    image: '/images/pexels-cottonbro-studio-5850889.jpg'
+  },
+  {
+    id: 4,
+    username: 'Syd Uwa',
+    image: '/images/pexels-andrea-piacquadio-839011.jpg'
+  }
+];
+
+export default function Followers() {
+  return (
+    <Box sx={{ boxShadow: 1, borderRadius: 2, mt: 3 }}>
+      <List>
+        {followers.map(follower => (
+          <ListItem key={follower.id}>
+            <ListItemButton href='#'>
+              <ListItemAvatar>
+                <Avatar alt={follower.username} src={follower.image} />
+              </ListItemAvatar>
+              <ListItemText color='soot'>{follower.username}</ListItemText>
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+}
--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,10 +1,12 @@
 import { Box } from '@mui/material';
+import Followers from './followers';
 import Tags from './tags';
 
 export default function Sidebar() {
   return (
     <Box component='aside'>
       <Tags />
+      <Followers />
     </Box>
   );
 }
