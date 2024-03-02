e1af96cebe405be3a113f7d9fb44701aa1edccaf
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2024-03-01 19:03:58 -0500

    main app feed

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 'use client';
 import FeedFilter from '@/components/feedFilter';
+import MainFeed from '@/components/mainFeed';
 import { Box } from '@mui/material';
 
 export default function Home() {
   return (
     <Box>
       <FeedFilter />
+      <MainFeed />
     </Box>
   );
 }
