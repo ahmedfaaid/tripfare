81b9e4235f241b06f403292bf5bfcd5e5aabdd54
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2024-03-24 18:36:48 -0400

    cookie provider

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,12 @@
+'use client';
 import Navbar from '@/components/navbar';
 import Sidebar from '@/components/sidebar';
+import AuthProvider from '@/context/auth';
 import theme from '@/theme';
 import { Box, Container, CssBaseline, Grid } from '@mui/material';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
 import { ThemeProvider } from '@mui/material/styles';
-
-export const metadata = {
-  title: 'Trip Fare'
-};
+import { CookiesProvider } from 'react-cookie';
 
 export default function RootLayout({ children }) {
   return (
@@ -16,19 +15,23 @@ export default function RootLayout({ children }) {
         <AppRouterCacheProvider>
           <ThemeProvider theme={theme}>
             <CssBaseline />
-            <Navbar />
-            <Container sx={{ my: 5 }}>
-              <Grid container columnSpacing={12}>
-                <Grid item md={4}>
-                  <Sidebar />
-                </Grid>
-                <Grid item md={8}>
-                  <Box component='main' sx={{ minHeight: '100vh' }}>
-                    {children}
-                  </Box>
-                </Grid>
-              </Grid>
-            </Container>
+            <CookiesProvider>
+              <AuthProvider>
+                <Navbar />
+                <Container sx={{ my: 5 }}>
+                  <Grid container columnSpacing={12}>
+                    <Grid item md={4}>
+                      <Sidebar />
+                    </Grid>
+                    <Grid item md={8}>
+                      <Box component='main' sx={{ minHeight: '100vh' }}>
+                        {children}
+                      </Box>
+                    </Grid>
+                  </Grid>
+                </Container>
+              </AuthProvider>
+            </CookiesProvider>
           </ThemeProvider>
         </AppRouterCacheProvider>
       </body>
