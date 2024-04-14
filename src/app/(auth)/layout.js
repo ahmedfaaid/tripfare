7fe46062367a334d001e48a509bcc627437c5f74
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2024-04-14 19:15:55 -0400

    redirect if user is already authenticated

--- src/app/(auth)/layout.js
+++ src/app/(auth)/layout.js
@@ -1,6 +1,19 @@
+import { AuthContext } from '@/context/auth';
 import { Container } from '@mui/material';
+import { useParams, useRouter } from 'next/navigation';
+import { useContext, useEffect } from 'react';
 
 export default function LoginDashboard({ children }) {
+  const { user } = useContext(AuthContext);
+  const router = useRouter();
+  const params = useParams();
+
+  useEffect(() => {
+    if (user) {
+      router.replace(params.redirect ? `${params.redirect}` : '/');
+    }
+  }, [user, params.redirect, router]);
+
   return (
     <Container
       component='section'
