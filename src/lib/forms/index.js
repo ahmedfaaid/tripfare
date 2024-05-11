261ec213da688336db139c276b3ba4d7a6019773
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2024-05-10 20:23:25 -0400

    add register and address schema

--- src/lib/forms/index.js
+++ src/lib/forms/index.js
@@ -1,3 +1,4 @@
+import { supported_formats } from '@/utils/constants';
 import * as Yup from 'yup';
 
 export const loginSchema = Yup.object().shape({
@@ -5,4 +6,43 @@ export const loginSchema = Yup.object().shape({
   password: Yup.string()
     .required('Please enter your password')
     .min(4, 'Password is too short')
+});
+
+const addressSchema = Yup.object().shape({
+  line_1: Yup.string().required(),
+  line_2: Yup.string().notRequired(),
+  city: Yup.string().required(),
+  state: Yup.string().required(),
+  country: Yup.string().required()
+});
+
+export const registerSchema = Yup.object().shape({
+  first_name: Yup.string().min(2).required(),
+  last_name: Yup.string().min(2).required(),
+  email: Yup.string().email('Please enter a valid email').required(),
+  username: Yup.string().min(6).max(16).required(),
+  password: Yup.string()
+    .required('Please enter your password')
+    .min(4, 'Password is too short'),
+  confirm_password: Yup.string().oneOf(
+    [Yup.ref('password'), null],
+    'Passwords do not match'
+  ),
+  date_of_birth: Yup.date().required(),
+  occupation: Yup.string().required(),
+  gender: Yup.string().oneOf(['male', 'female']),
+  address: addressSchema,
+  profile_picture: Yup.mixed()
+    .nullable()
+    .notRequired()
+    .test(
+      'fileSize',
+      'File exceeds the maximum supported size of 14 MB',
+      (value) => value && value.size <= 1024 * 1024 * 14
+    )
+    .test(
+      'format',
+      'Supported file types: ".jpeg", ".jpg" and ".png"',
+      (value) => value && supported_formats.includes(value.type)
+    )
 });
