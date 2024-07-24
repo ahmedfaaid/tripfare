53419716bc606266e33bb448d711c037b8b6933d
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2024-07-24 13:54:51 -0400

    quick cache data

--- src/hooks/useGetUserPosts.js
+++ src/hooks/useGetUserPosts.js
@@ -1,35 +1,44 @@
 import { apiUrl } from '@/utils/constants';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 export default function useGetUserPosts(username, type) {
   const [loading, setLoading] = useState(false);
   const [posts, setPosts] = useState();
   const [error, setError] = useState(null);
+  const cache = useRef({});
+
+  const url = `${apiUrl}/post?username=${username}&type=${type}`;
 
   useEffect(() => {
     const fetchPosts = async () => {
       setLoading(true);
 
-      const req = await fetch(
-        `${apiUrl}/post?username=${username}&type=${type}`
-      );
-      const res = await req.json();
+      if (cache.current[url]) {
+        const data = cache.current[url];
+        setError(null);
+        setPosts(data);
+        setLoading(false);
+        return;
+      } else {
+        const req = await fetch(url);
+        const res = await req.json();
 
-      if (res.statusCode === 400 || res.statusCode === 404) {
-        setError(res);
-        setPosts(null);
+        if (res.statusCode === 400 || res.statusCode === 404) {
+          setError(res);
+          setPosts(null);
+          setLoading(false);
+          return;
+        }
+
+        setError(null);
+        setPosts(res);
         setLoading(false);
         return;
       }
-
-      setError(null);
-      setPosts(res);
-      setLoading(false);
-      return;
     };
 
     fetchPosts();
-  }, [username, type]);
+  }, [url]);
 
   return { loading, error, posts };
 }
